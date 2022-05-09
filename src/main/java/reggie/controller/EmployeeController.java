4a2fb75bbc4b744929ac4a5809fda5a274785ef9
YRR <fdgs2012@gmail.com>
2022-05-09 20:14:39 +0800

    项目搭建

---
+++ src/main/java/reggie/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package reggie.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import reggie.common.R;
+import reggie.entity.Employee;
+import reggie.service.EmployeeService;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    /**
+     * 员工登录
+     * @param request
+     * @param employee
+     * @return
+     */
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+        return null;
+    }
+}
