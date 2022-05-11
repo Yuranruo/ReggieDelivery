76a66dbee30f1c2ccd2184e0d79227b8f4945d50
YRR <fdgs2012@gmail.com>
2022-05-11 16:57:52 +0800

    菜品管理环境搭建

---
+++ src/main/java/reggie/controller/DishController.java
@@ -0,0 +1,21 @@
+package reggie.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reggie.service.DishFlavorService;
+import reggie.service.DishService;
+
+@Slf4j
+@RestController
+@RequestMapping("/dish")
+public class DishController {
+
+    @Autowired
+    private DishService dishService;
+    @Autowired
+    private DishFlavorService dishFlavorService;
+
+
+}
