69dd55dc2d1dc46c50a33b4793f9bf32b72db83e
YRR <fdgs2012@gmail.com>
2022-05-11 17:02:15 +0800

    菜品分页查询

--- src/main/java/reggie/controller/DishController.java
+++ src/main/java/reggie/controller/DishController.java
@@ -1,9 +1,15 @@
 package reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reggie.common.R;
+import reggie.entity.Category;
+import reggie.entity.Dish;
 import reggie.service.DishFlavorService;
 import reggie.service.DishService;
 
@@ -17,5 +23,28 @@ public class DishController {
     @Autowired
     private DishFlavorService dishFlavorService;
 
+    /**
+     * 菜品信息分页查询
+     *
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize) {
+        log.info("page = {}, pageSize = {}", page, pageSize);
+
+        //构造分页构造器
+        Page pageInfo = new Page(page, pageSize);
+
+        //构造条件构造器: 排序条件
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByAsc(Dish::getUpdateTime);
+
+        //执行查询
+        dishService.page(pageInfo, queryWrapper);
+
+        return R.success(pageInfo);
+    }
 
 }
