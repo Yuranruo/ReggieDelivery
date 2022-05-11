b186e0a4894c0567c71bf768865235c863a0e522
YRR <fdgs2012@gmail.com>
2022-05-11 12:58:32 +0800

    新增套餐分类及分页查询

---
+++ src/main/java/reggie/controller/CategoryController.java
@@ -0,0 +1,68 @@
+package reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.*;
+import reggie.common.R;
+import reggie.entity.Category;
+import reggie.entity.Employee;
+import reggie.service.CategoryService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 分类管理
+ */
+@Slf4j
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 新增分类
+     *
+     * @param category
+     * @return
+     */
+    @PostMapping
+    public R<String> save(@RequestBody Category category) {
+        log.info("新增分类, 分类信息: {}", category.toString());
+        //保存分类
+        categoryService.save(category);
+
+        return R.success("新增分类成功");
+    }
+
+    /**
+     * 套餐信息分页查询
+     *
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize) {
+        log.info("page = {}, pageSize = {}", page, pageSize);
+
+        //构造分页构造器
+        Page pageInfo = new Page(page, pageSize);
+
+        //构造条件构造器: 排序条件
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByAsc(Category::getSort);
+
+        //执行查询
+        categoryService.page(pageInfo, queryWrapper);
+
+        return R.success(pageInfo);
+    }
+
+}
---
+++ src/main/java/reggie/service/CategoryService.java
@@ -0,0 +1,7 @@
+package reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import reggie.entity.Category;
+
+public interface CategoryService extends IService<Category> {
+}
