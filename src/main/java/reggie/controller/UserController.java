cfa996249f9562765216ae5ba8f932e63b6a3902
YRR <fdgs2012@gmail.com>
2022-05-13 21:50:49 +0800

    手机验证码&手机登录环境搭建

---
+++ src/main/java/reggie/controller/UserController.java
@@ -0,0 +1,17 @@
+package reggie.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reggie.service.UserService;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+}
