368a5d85384741481a6877c029260a858b5e1dfa
YRR <fdgs2012@gmail.com>
2022-05-14 14:53:31 +0800

    发送短信验证码,并实现移动端手机号登录功能

--- src/main/java/reggie/controller/UserController.java
+++ src/main/java/reggie/controller/UserController.java
@@ -1,10 +1,20 @@
 package reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reggie.common.R;
+import reggie.entity.User;
 import reggie.service.UserService;
+import reggie.utils.SMSUtils;
+import reggie.utils.ValidateCodeUtils;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -13,5 +23,79 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    /**
+     * 发送手机验证码
+     *
+     * @param user
+     * @return
+     */
+    @RequestMapping("/sendMsg")
+    public R<String> sendMsg(@RequestBody User user, HttpSession session) {
+        log.info(user.toString());
+
+        //获取手机号
+        String phone = user.getPhone();
+        //短信服务的AppCode
+        String AppCode = "";
+
+        if (StringUtils.isNotEmpty(phone)) {
+            //生成验证码
+            String code = ValidateCodeUtils.generateValidateCode(4).toString();
+            //调用阿里云api发送短信
+
+//            SMSUtils.sendMessage(AppCode, phone, code);   //调用阿里云发送短信的api
+            log.info("code={}", code);
+
+            //将生成的验证码保存到session
+            session.setAttribute(phone, code);
+
+            return R.success("短信发送成功");
+
+        }
+        return R.error("短信发送失败");
+    }
+
+    /**
+     * 移动端用户登录
+     *
+     * @param map
+     * @return
+     */
+    @RequestMapping("/login")
+    public R<User> login(@RequestBody Map map, HttpSession session) {
+        log.info(map.toString());
+
+        //获取手机号
+        String phone = map.get("phone").toString();
+
+        //获取验证码
+        String code = map.get("code").toString();
+
+        //从session中获取保存的验证码
+        String sessionCode = session.getAttribute(phone).toString();
+
+        //进行验证码比对(页面提交的验证码和session中保存的验证码比对)
+        if (StringUtils.isNotEmpty(sessionCode) && code.equals(sessionCode)) {
+            //如果比对成功, 说明登录成功
+            LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(User::getPhone, phone);
+            User user = userService.getOne(queryWrapper);
+            if (user == null) {
+                //判断当前手机号对应的用户是否为新用户, 如果是新用户就自动完成注册
+                user = new User();
+                user.setPhone(phone);
+                user.setStatus(1);
+                userService.save(user);
+            }
+
+            //将user id放入session中
+            session.setAttribute("user", user.getId());
+
+            return R.success(user);
+        }
+
+        return R.error("验证码错误, 登陆失败");
+    }
 
 }
--- src/main/java/reggie/utils/SMSUtils.java
+++ src/main/java/reggie/utils/SMSUtils.java
@@ -6,35 +6,86 @@ import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
 import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
 import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.profile.DefaultProfile;
+import org.apache.http.HttpResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * 短信发送工具类
  */
 public class SMSUtils {
 
-	/**
-	 * 发送短信
-	 * @param signName 签名
-	 * @param templateCode 模板
-	 * @param phoneNumbers 手机号
-	 * @param param 参数
-	 */
-	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
-		IAcsClient client = new DefaultAcsClient(profile);
+    /**
+     * 发送短信的utils, 改用阿里云市场买的短信服务的代码
+     *
+     * @param appcode      阿里云购买的短信服务的appcode
+     * @param phoneNumbers 手机号
+     * @param verifyCode   验证码
+     */
+    public static void sendMessage(String appcode, String phoneNumbers, String verifyCode) {
+        String host = "https://miitangs09.market.alicloudapi.com";
+        String path = "/v1/tools/sms/code/sender";
+        String method = "POST";
+        Map<String, String> headers = new HashMap<String, String>();
+        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+        headers.put("Authorization", "APPCODE " + appcode);
+        //根据API的要求，定义相对应的Content-Type
+        headers.put("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        //需要给X-Ca-Nonce的值生成随机字符串，每次请求不能相同
+        headers.put("X-Ca-Nonce", UUID.randomUUID().toString());
+        Map<String, String> querys = new HashMap<String, String>();
+        Map<String, String> bodys = new HashMap<String, String>();
+        bodys.put("phoneNumber", phoneNumbers);
+        bodys.put("reqNo", "222");    //请求号, 不知道干啥用的, 不确定是不是能写死
+        bodys.put("smsSignId", "0000");    //默认的注册id, 自己新建需要发邮件
+        bodys.put("smsTemplateNo", "0003");    //默认模板编号
+        bodys.put("verifyCode", verifyCode);
 
-		SendSmsRequest request = new SendSmsRequest();
-		request.setSysRegionId("cn-hangzhou");
-		request.setPhoneNumbers(phoneNumbers);
-		request.setSignName(signName);
-		request.setTemplateCode(templateCode);
-		request.setTemplateParam("{\"code\":\""+param+"\"}");
-		try {
-			SendSmsResponse response = client.getAcsResponse(request);
-			System.out.println("短信发送成功");
-		}catch (ClientException e) {
-			e.printStackTrace();
-		}
-	}
 
+        try {
+            /**
+             * 重要提示如下:
+             * HttpUtils请从
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
+             * 下载
+             *
+             * 相应的依赖请参照
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+             */
+            HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
+            System.out.println(response.toString());
+            //获取response的body
+            //System.out.println(EntityUtils.toString(response.getEntity()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 发送短信
+     * @param signName 签名
+     * @param templateCode 模板
+     * @param phoneNumbers 手机号
+     * @param param 参数
+     */
+//	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
+//		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
+//		IAcsClient client = new DefaultAcsClient(profile);
+//
+//		SendSmsRequest request = new SendSmsRequest();
+//		request.setSysRegionId("cn-hangzhou");
+//		request.setPhoneNumbers(phoneNumbers);
+//		request.setSignName(signName);
+//		request.setTemplateCode(templateCode);
+//		request.setTemplateParam("{\"code\":\""+param+"\"}");
+//		try {
+//			SendSmsResponse response = client.getAcsResponse(request);
+//			System.out.println("短信发送成功");
+//		}catch (ClientException e) {
+//			e.printStackTrace();
+//		}
+//	}
 }
